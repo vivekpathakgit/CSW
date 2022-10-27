c9a472c695accf7091de7682bae8c136d93473e5
vivekpathakgit <93838914+vivekpathakgit@users.noreply.github.com>
2022-10-27 16:36:36 +0530

    Add files via upload

---
+++ 2141016163_VivekPathak/js1/index.js
@@ -0,0 +1,12 @@
+
+var name = prompt("Enter your name");
+console.log(name.length);
+document.getElementsByTagName("h1")[0].style.color="red";
+console.log(document.getElementsByTagName("h1").length);
+console.log(document.getElementById("e1"));
+document.getElementsByTagName("title")[0].innerHTML="Welcome";
+document.getElementById("l1").style.color="orange";
+
+document.querySelector("h1").style.fontSize="10rem";
+
+
