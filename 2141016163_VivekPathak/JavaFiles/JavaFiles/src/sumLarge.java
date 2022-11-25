da80a99efcfcabbd81cf3908ac189ff38ae7252b
Vivek Pathak <93838914+vivekpathakgit@users.noreply.github.com>
2022-11-25 12:27:02 +0530

    Update sumLarge.java

--- 2141016163_VivekPathak/JavaFiles/JavaFiles/src/sumLarge.java
+++ 2141016163_VivekPathak/JavaFiles/JavaFiles/src/sumLarge.java
@@ -8,24 +8,21 @@ public class sumLarge {
 		String s1 = sc.next();
 		String s2 = sc.next();
 		int carry = 0;
-		int i1 = s1.length(), i2 = s2.length();
+		int i1 = s1.length()-1, i2 = s2.length()-1;
 		String res = "";
 		while(i1>=0 && i2>=0) {
-			res +=(char)(carry + s1.charAt(i1)+s2.charAt(i2))%10 ;
-			carry = (carry + s1.charAt(i1)+s2.charAt(i2))/10;
+			res =(char)((carry + s1.charAt(i1)+s2.charAt(i2) - 2*48)%10+48) + res ;
+			carry = (carry + s1.charAt(i1--)+s2.charAt(i2--)-2*48)/10;
 		}
 		while(i1>=0) {
-			res += (char)(carry + s1.charAt(i1))%10;
-			carry = (carry + s1.charAt(i1))/10;
+			res = ((char)((carry + s1.charAt(i1)-48)%10 +48)) + res;
+			carry = (carry + s1.charAt(i1--) - 48)/10;
 		}
 		while(i2>=0) {
-			res += (char)(carry + s1.charAt(i2))%10;
-			carry = (carry + s1.charAt(i2))/10;
+			res = ((char)((carry + s2.charAt(i2)-48)%10+48))+ res;
+			carry = (carry + s1.charAt(i2--)- 48 )/10;
 		}
-		String res2 = "";
-		for(int i = res.length()-1; i>=0; i--) {
-			res2 += res.charAt(i);
-		}
+		System.out.println(res);
 	}
 
 }
